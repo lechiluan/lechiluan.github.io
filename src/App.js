e8db1b476e74196925267834d170df63f3e2478b
lechiluan <chiluan6601@gmail.com>
2024-08-03 21:05:19 +0700

    commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
-import logo from "./logo.svg";
+import React from "react";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import BlogList from "./components/BlogList";
 import "./App.css";
 
 function App() {
+  const posts = [
+    { title: "First Post", content: "This is my first blog post!" },
+    { title: "Second Post", content: "This is my second blog post!" },
+  ];
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h1>Coming Soon!!!</h1>
-        <p>
-          I want to build a website that will help you to learn programming.
-        </p>
-        <a
-          className="App-link"
-          href="https://github.com/lechiluan"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Connect me on GitHub
-        </a>
-      </header>
+      <Header />
+      <BlogList posts={posts} />
+      <Footer />
     </div>
   );
 }
